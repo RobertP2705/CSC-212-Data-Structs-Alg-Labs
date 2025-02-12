eea69cf75da74a1eefbf2688710ee8a606d06d69
RobertP2705 <136493854+RobertP2705@users.noreply.github.com>
2025-02-12 17:09:47 -0600

    qwe

---
+++ Lab-5-main/CircularLinkedList.java
@@ -0,0 +1,38 @@
+public class CircularLinkedList<T> {
+    private static class Node<T>{
+        private T data;
+        private Node<T> next;
+
+        private Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    private Node<T> head;
+    public Node<T> currentNode;
+    public CircularLinkedList(){
+        this.head = null;
+        this.currentNode = null;
+    }
+    public void append(T data){
+        Node<T> newNode = new Node<T>(data);
+        if(this.head == null){
+            this.head = newNode;
+            this.currentNode = newNode;
+            this.head.next = this.head;
+        }
+        else{
+            Node<T> temp = this.head;
+            while(temp.next != this.head){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.next = this.head;
+        }
+    }
+    public void step(){
+        if(this.head != null){
+            this.currentNode = this.currentNode.next;
+        }
+    }
+}
